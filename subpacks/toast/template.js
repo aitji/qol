3f73d2eb92296e0a921a9af5f0f7c6652358cf2e
aitji <ait.suriya@gmail.com>
2025-12-03 09:22:40 +0700

    current version

--- subpacks/toast/template.js
+++
@@ -1,6 +0,0 @@
-import { system } from "@minecraft/server"
-export function sendNotification(player, message, playSound = true) {
-    if (playSound) player.playSound("random.toast_recipe_unlocking_in")
-    player.onScreenDisplay.setTitle(`notification.${message}`)
-    if (playSound) system.runTimeout(() => player?.isValid() && player.playSound("random.toast_recipe_unlocking_out"), 108)
-}
